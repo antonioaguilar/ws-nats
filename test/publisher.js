62a05023e077a742259b7dffa4b5e30df8e96a29
Antonio Aguilar <antonioaguilar@users.noreply.gitlab.com>
2018-10-04 17:30:19 +0100

    release v1.0.0

---
+++ test/publisher.js
@@ -0,0 +1,30 @@
+var NATS = require('../dist/ws-nats.cjs').NATS;
+
+var nats = NATS.connect({ url: 'ws://0.0.0.0:8080', json: true, name: 'ws-nats:nodejs' });
+
+console.log('NATS library version:', NATS.version);
+
+function random (min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
+
+var count = 0, MAX_MESSAGES = 10000;
+
+var timer = setInterval(function() {
+
+  if (count < MAX_MESSAGES) {
+    var id = Date.now();
+    var payload = {
+      id: id,
+      count: count,
+      cpu: random(10, 100),
+      timestamp: new Date().toISOString()
+    };
+    nats.publish('events.' + id, payload);
+    console.log(count, JSON.stringify(payload, null, 0));
+    count++;
+  }
+  else {
+    clearInterval(timer);
+    process.exit();
+  }
+
+}, 10);
