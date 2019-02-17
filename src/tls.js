84c1a352261568f75412bcf30aadee01abb7a978
Antonio Aguilar <antonioaguilar@users.noreply.github.com>
2019-02-17 01:42:12 +0000

    added webpack and rollup build scripts

--- src/tls.js
+++ src/tls.js
@@ -1,46 +1,22 @@
-var url_parse = require('url').parse;
-var env = require('./env');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
-
-var WebSocket = env.isNode() ? require('websocket').w3cwebsocket : window.WebSocket;
 
 function WebSocketProxy (url) {
   var self = this;
   EventEmitter.call(this);
-
-  if (env.isNode()) {
-    this.sock = new WebSocket(url, '', '*');
-
-    this.sock.onopen = function() {
-      self.emit('connect');
-    };
-    this.sock.onmessage = function(e) {
-      self.emit('data', new Buffer(e.data));
-    };
-    this.sock.onerror = function(e) {
-      self.emit('error', e);
-    };
-    this.sock.onclose = function() {
-      self.emit('close');
-    };
-  }
-  else {
-    this.sock = new WebSocket(url);
-    this.sock.addEventListener('open', function() {
-      self.emit('connect');
-    });
-    this.sock.addEventListener('message', function(e) {
-      self.emit('data', new Buffer(e.data));
-    });
-    this.sock.addEventListener('error', function(e) {
-      self.emit('error', e);
-    });
-    this.sock.addEventListener('close', function() {
-      self.emit('close');
-    });
-  }
-
+  this.sock = new WebSocket(url);
+  this.sock.addEventListener('open', function() {
+    self.emit('connect');
+  });
+  this.sock.addEventListener('message', function(e) {
+    self.emit('data', new Buffer(e.data));
+  });
+  this.sock.addEventListener('error', function(e) {
+    self.emit('error', e);
+  });
+  this.sock.addEventListener('close', function() {
+    self.emit('close');
+  });
 }
 
 util.inherits(WebSocketProxy, EventEmitter);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,3 @@
-var webpack = require('webpack');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
 var browserConfig = {
 
   entry: {
@@ -17,46 +14,8 @@ var browserConfig = {
     globalObject: 'this'
   },
 
-  optimization: {
-    minimizer: [
-      new UglifyJSPlugin({
-        sourceMap: true,
-        uglifyOptions: {
-          output: { comments: false },
-          compress: {
-            inline: false
-          }
-        }
-      })
-    ]
-  },
-
-  devtool: false,
-
-  plugins: [
-    // new webpack.HotModuleReplacementPlugin()
-    new webpack.SourceMapDevToolPlugin({})
-  ]
-
-};
-
-var nodejsConfig = {
-
-  entry: {
-    NATS: __dirname + '/src/nats.js'
-  },
-
-  target: 'node',
-
-  output: {
-    filename: 'ws-nats.cjs.js',
-    libraryTarget: 'commonjs',
-    path: __dirname + '/dist'
-  },
-
   plugins: []
 
 };
 
-// module.exports = [nodejsConfig, browserConfig];
 module.exports = [browserConfig];
